470e483e67fc5ecf811e586b03d52152270cafa8
Iman0har <manoharrao567@gmail.com>
2021-12-14 12:10:34 +0530

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    return `<img id="logo"  alt="">  
+            <a href="" class="navtext">Dashboard</a>
+            <a href="" class="navtext">Discover</a>
+            <a href="" class="navtext">Create</a>
+            <a href="" class="navtext" id="separation">Watchlist</a>
+            <a href="" class="navtext">Investments</a>
+           <a href="" class="navbut" ><i class="fas fa-bell"></i></a> 
+            <a href="" class="navbut" id="navq" >?</a>
+       
+         
+          <a href="" class="navbut">sahil<i class="fas fa-angle-down"></i></a>`
+}export default navbar
