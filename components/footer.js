b1d8c3cb16f4815402ea3c3060b4f241a65602d9
Iman0har <manoharrao567@gmail.com>
2021-12-18 21:33:20 +0530

    push

---
+++ components/footer.js
@@ -0,0 +1,32 @@
+function footer(){
+    return `   <div id="smallFooter">
+    <div id="upPart">
+      <div class="Fcolor">
+        © Powered by smallcase Technologies Pvt. Ltd. Email us at
+        <span>support@upstox.com</span>
+      </div>
+      <div id="myOptions">
+        <div>Help</div>
+        <div>Disclosures</div>
+        <div>More ^</div>
+      </div>
+    </div>
+    <div id="downPart">
+      <div>
+        <img
+          id="upstoxLogo"
+          src="https://lever-client-logos.s3.us-west-2.amazonaws.com/eb7738e7-d908-42f5-97da-61b28e53ce73-1631634736627.png"
+          alt=""
+        />
+        <p class="Fcolor">NSE & BSE - SEBI Registration No: INZ000185137</p>
+      </div>
+      <div>
+        <img
+          id="secure"
+          src="https://assets.smallcase.com/images/brokers/upstox/secure.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>`
+}export default footer
