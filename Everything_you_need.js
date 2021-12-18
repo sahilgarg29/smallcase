a023aabe8868cd851d2a1f4429a49d4a98a2cd8b
keshav-babu <keshavbabu742@gmail.com>
2021-12-18 18:45:50 +0530

    assigned work on landing page

---
+++ Everything_you_need.js
@@ -0,0 +1,43 @@
+function everythingYouNeed(){
+    return `
+    <div id="loop_intro">
+    <div>
+        <p style="color: grey;" >Everything you need to invest</p>
+        <h1>Simple, quick & delightful <br/>investing for you</h1>
+        <div id="loop_intro_div_div">
+            <div>
+                <div style="display: flex; gap: 10px;">
+                    <img src="https://www.smallcase.com/static/images/svg/like.svg" alt="" style="height: 30px; width: 30px; border: 1px solid green;">
+                    <h4 style="margin-top: 3px;">Buy multiple stocks in 1 click</h4>
+                </div>
+                <p id="p1" class="loop_intro_div_div1_p">Place orders for up to 50 stocks in 1 click as a smallcase and save time</p>
+            </div>
+            <div>
+                <div style="display: flex; gap: 10px;">
+                    <img src="https://www.smallcase.com/static/images/svg/track.svg" alt="" style="height: 30px; width: 30px; border: 1px solid green;">
+                    <h4 style="margin-top: 3px;">Track smallcases anytime, anywhere</h4>
+                </div>
+                <p id="p2" class="loop_intro_div_div1_p">Track & manage seamlessly with a real time index value, inclusive of corp. actions & dividends</p>
+            </div>
+            <div>
+                <div style="display: flex; gap: 10px;">
+                    <img src="https://www.smallcase.com/static/images/svg/coffee.svg" alt="" style="height: 30px; width: 30px; border: 1px solid green;">
+                    <h4 style="margin-top: 3px;">Track smallcases anytime, anywhere</h4>
+                </div>
+                <!-- <p  class="loop_intro_div_div1_p">Track & manage seamlessly with a real time index value, inclusive of corp. actions & dividends</p> -->
+            </div>
+            <div>
+                <div style="display: flex; gap: 10px;">
+                    <img src="https://www.smallcase.com/static/images/svg/cube.svg" alt="" style="height: 30px; width: 30px; border: 1px solid green;">
+                    <h4 style="margin-top: 3px;">Create your own smallcase</h4>
+                </div>
+                <!-- <p  class="loop_intro_div_div1_p">Be your own fund manager & invest in your ideas by creating a smallcase of up to 50 stocks</p> -->
+            </div>
+        
+        </div>
+    </div>
+    <iframe id="loop_video" src="https://assets.smallcase.com/smallcase/assets/videos/invest_new.mp4" frameborder="0"></iframe>
+</div>
+    `
+}
+export default everythingYouNeed;
---
+++ curate_by_india.js
@@ -0,0 +1,24 @@
+function curateByIndia(){
+    return `
+    <div id="mid">
+        <div id="mid_box">
+            
+            <iframe  id="mid_box_video"  src="https://assets.smallcase.com/smallcase/assets/videos/creator-scroll.mp4" frameborder="0"></iframe>
+            <div>
+                <h1>Curated by India's <br/>
+                    top investment managers</h1>
+                <p>smallcases are created & managed
+                     by leading professionals who are
+
+                      <span style="font-weight: bold;">SEBI-licensed</span> research analysts
+                       and investment advisors</p>
+                <div style="display: flex; gap:50px;">
+                <button style="background-color: blue; color: white; border-radius: 5px; padding: 10px;">View All Managers</button>
+                <button style="background-color: white; color: blue; border-radius: 5px; padding: 10px;">View case by Case</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+}
+export default curateByIndia;
