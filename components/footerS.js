e524434e59a5cd6d19bbbef1662f321bf42591e9
Rohit8483 <93434901+Rohit8483@users.noreply.github.com>
2021-12-18 17:33:46 +0530

    body=discover page,windmill=discover-windmill
    body=discover page,windmill page=discover-windmill

---
+++ components/footerS.js
@@ -0,0 +1,68 @@
+function smallFooter() {
+  return `  
+
+    <div id="smallFooter">
+      <div id="upPart">
+        <div class="Fcolor">
+          © Powered by smallcase Technologies Pvt. Ltd. Email us at
+          <span>support@upstox.com</span>
+        </div>
+        <div id="myOptions">
+          <div>Help</div>
+          <div>Disclosures</div>
+          <div>More ^</div>
+        </div>
+      </div>
+      <div id="downPart">
+        <div>
+          <img
+            id="upstoxLogo"
+            src="https://lever-client-logos.s3.us-west-2.amazonaws.com/eb7738e7-d908-42f5-97da-61b28e53ce73-1631634736627.png"
+            alt=""
+          />
+          <p class="Fcolor">NSE & BSE - SEBI Registration No: INZ000185137</p>
+        </div>
+        <div>
+          <img
+            id="secure"
+            src="https://assets.smallcase.com/images/brokers/upstox/secure.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>`;
+}
+// export default smallFooter;
+
+function upperPart() {
+  return `<div id="myBody">
+  <div id="body1">
+    <div id="overview">
+      <h1>Overview</h1>
+      <p>Markets & your investments</p>
+    </div>
+    <div id="niftyH">
+      <h3 class="nifty">NIFTY</h3>
+      <h4 id="rate">
+        17,245.80
+        <i class="fa fa-caret-down" style="font-size: 26px; color: red"></i>
+          <span id="percentage"> 0.70%</span>
+        </i>
+      </h4>
+    </div>
+    <div>
+      <h3 class="nifty">Current Value</h3>
+      <h1>-</h1>
+    </div>
+    <div>
+      <h3 class="nifty">Current Returns</h3>
+      <h1>-</h1>
+    </div>
+    <div id="discoverbtn">
+      <button id="discoverbutton">Discover smallcases</button>
+    </div>
+  </div>
+</div>`;
+}
+
+export { smallFooter, upperPart };
