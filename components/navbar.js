fac0c48bbc5b0d60c2356df61b18fdacd158175e
sahil garg <gargs2483@gmail.com>
2021-12-19 12:32:45 +0530

    merge keshav branch

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,7 @@ function navbar() {
             <a href="discover.html" class="navtext">Discover</a>
             <a href="creative.html" class="navtext">Create</a>
             <a href="watchlist.html" class="navtext" id="separation">Watchlist</a>
-            <a href="" class="navtext">Investments</a>
+            <a href="investment.html" class="navtext">Investments</a>
            <a href="" class="navbut" ><i class="fas fa-bell"></i></a> 
             <a href="" class="navbut" id="navq" >?</a>
        
