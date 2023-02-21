d7da8650459e6ca3c22bd44ff7eeee6cfa7b9e47
sahil garg <gargs2483@gmail.com>
2023-02-21 16:26:47 +0530

    env change

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-const app = require("./app");
-const connect = require("./configs/db");
+const app = require('./app');
+const connect = require('./configs/db');
 
-app.listen(5001, async function () {
-  try {
-    await connect();
-    console.log("listening on port 2345");
-  } catch (err) {
-    console.log(err);
-  }
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, async function () {
+	try {
+		await connect();
+		console.log('listening on port 2345');
+	} catch (err) {
+		console.log(err);
+	}
 });
